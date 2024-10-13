9c578d6b2174228a30892ee19df153d56a67a21a
norma <normanmagic2004@gmail.com>
2024-10-13 10:56:10 +0300

    $

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header/Header.jsx";
-import Me from "./components/Me/Me.jsx";
-import WorkCards from "./components/WorkCards/WorkCards.jsx";
-import Skills from "./components/Skils/Skils.jsx";
-import Projects from "./components/Projects/Projects.jsx";
-import Footer from "./components/Footer/Footer.jsx";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Header />
-
-    <Me />
-    <WorkCards />
-    <Skills />
-    <Projects />
-
-    <Footer />
-  </React.StrictMode>
+  <>
+   <App />
+  </>
 );
